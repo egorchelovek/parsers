9034afaad018338bb5d0ea2e8dfd60ea3a2cb7a8
Egor <egor.zamotaev@mail.ru>
2019-09-13 10:01:49 +0700

    Add workers list page

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from app.models import Worker
 
 # Create your views here.
+def workers_list(request):
+    workers = Worker.objects.all();
+    return render(request, "app/workers_list.html", {'workers':workers})
